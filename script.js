31140eb341bd0d01a745f1b3b47abba629281029
mariaclara914 <maria.burilho@escola.pr.gov.br>
2026-06-02 20:11:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", function(){
+
+
+document.getElementById("problema")
+.scrollIntoView({
+    behavior: "smooth"
+});
+
+
+});
